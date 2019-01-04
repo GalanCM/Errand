9a649e4c90f1c0b8bf71a7da7f7a02bbb93b30b1
Galan Montgomery <galan.c.m@gmail.com>
2019-01-04 18:23:43 -0500

    Remove remaining reference to FuzzyPlanner.

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,12 +8,11 @@
       <main>
         <p>
           Unfortunately, most of the web apps that I've written in the past are either locked away behind corporate ethernets, or defunct.
-          To make up for this hole in my portfolio, and to prove my ability to write web applications to potential clients and employers, I built this simple organizer. 
-          If you're interested in the code, <a href="https://github.com/GalanCM/FuzzyPlanner">it is available on Github</a>.
+          To make up for this hole in my portfolio, and to prove my ability to write web applications to potential clients and employers, I built this simple organizer.
+          If you're interested in the code,
+          <a href="https://github.com/GalanCM/Errand">it is available on Github</a>.
         </p>
-        <p>
-          I want to keep this tool simple, but elegant. The current version is a proof of concept, but can already handle five operations:
-        </p>
+        <p>I want to keep this tool simple, but elegant. The current version is a proof of concept, but can already handle five operations:</p>
         <ol>
           <li>Creating new tasks</li>
           <li>Editing task descriptions</li>
@@ -21,19 +20,25 @@
           <li>Reordering tasks</li>
           <li>Deleting tasks</li>
         </ol>
-        <p>
-          There is still work left for me to do, but the sooner I can show this to clients and employers, the better. The following tasks should be completed over the next couple weeks:
-        </p>
+        <p>There is still work left for me to do, but the sooner I can show this to clients and employers, the better. The following tasks should be completed over the next couple weeks:</p>
         <ul>
-          <li><strong>Server-side Saving.</strong> This is a lot more work than local saving, but I should probably flex my back-end skills.</li>
-          <li><strong>Better Keyboard Navigation.</strong> Currently, you can't really use the app without a mouse.</li>
-          <li><strong>Onboarding.</strong> Some light tutorials and tooltips would help new users learn the interface.</li>
-          <li><strong>User Testing.</strong> I have done very little user testing, and what little I have done has already suggested that I need to make some changes.</li>
-          <li><strong>Accessibility & Compatibility Pass.</strong> I want to make sure this is usable for as many people as possible.</li>
+          <li>
+            <strong>Server-side Saving.</strong> This is a lot more work than local saving, but I should probably flex my back-end skills.
+          </li>
+          <li>
+            <strong>Better Keyboard Navigation.</strong> Currently, you can't really use the app without a mouse.
+          </li>
+          <li>
+            <strong>Onboarding.</strong> Some light tutorials and tooltips would help new users learn the interface.
+          </li>
+          <li>
+            <strong>User Testing.</strong> I have done very little user testing, and what little I have done has already suggested that I need to make some changes.
+          </li>
+          <li>
+            <strong>Accessibility & Compatibility Pass.</strong> I want to make sure this is usable for as many people as possible.
+          </li>
         </ul>
-        <p>
-          Hopefully you like what you see. And whether you intend to hire me or not, I'd love to know what you think.
-        </p>
+        <p>Hopefully you like what you see. And whether you intend to hire me or not, I'd love to know what you think.</p>
       </main>
     </div>
   </div>
